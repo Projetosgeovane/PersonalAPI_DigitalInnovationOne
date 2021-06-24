753bd24a6dbe302151fc9131e922ad01666effe5
Geovane Silva <geovanesilva.nar@hotmail.com>
2021-06-24 06:36:20 -0300

    Inicio do projeto de gerenciamento de pessoas com API REST

---
+++ src/main/java/com/digitalinnovation/personalapi/PersonalapiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.personalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalinnovation/personalapi/PersonalapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.personalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
