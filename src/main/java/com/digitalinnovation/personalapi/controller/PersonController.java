50eb6b4952a90e7053da04ba8857d9e00c423650
Geovane Silva <geovanesilva.nar@hotmail.com>
2021-07-05 00:42:11 -0300

    Criação do método de buscar todos elementos da lista

--- src/main/java/com/digitalinnovation/personalapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovation/personalapi/controller/PersonController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -27,6 +28,12 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
     return personService.createPerson(personDTO);
+
+    }
+
+   @GetMapping
+    public List<PersonDTO> listAll(){
+        return personService.listAll();
 
     }
 
--- src/main/java/com/digitalinnovation/personalapi/service/PersonService.java
+++ src/main/java/com/digitalinnovation/personalapi/service/PersonService.java
@@ -10,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PersonService {
 
@@ -32,5 +35,12 @@ public class PersonService {
                 .message("Created person with ID " + savedPerson.getId())
                 .build();
 
+    }
+
+    public List<PersonDTO> listAll() {
+        List<Person> allPeople = personRepository.findAll();
+        return allPeople.stream()
+                .map(personMapper::toDTO)
+                .collect(Collectors.toList());
     }
 }
